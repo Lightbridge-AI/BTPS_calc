17c13a6c0cbf42ba9f062baf85855270bf2866f8
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2020-12-10 23:37:10 +0700

    add package , add theme

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("dplyr", "purrr")
+my_packages = c("dplyr", "purrr","shinythemes","openxlsx")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
